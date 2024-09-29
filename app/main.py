d3b27f70e9bd1be8385b9a865fa20997573e032d
virajsabhaya739 <virajsabhaya72@gmail.com>
2024-09-29 21:53:49 +0530

    REPL update

--- app/main.py
+++ app/main.py
@@ -1,17 +1,33 @@
 import sys
+import os
 
-valid_cs = []
+valid_cs = ["exit", "echo", "type"]
 
 def main():
+    PATH = os.environ.get("PATH")
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    command = input()
-    if command not in valid_cs:
-        print(f"{command}: command not found")
-    main()
+        # Wait for user input
+        command_full = input()
+
+        cmd_split = command_full.split()
+        # print(cmd_split[0])
+
+        if cmd_split[0] not in valid_cs:
+            command_not_found(command_full)
+        else:
+            command_not_found(command_full)
+
+
+def command_not_found(cmd: str) -> None:
+    print(f"{cmd}: command not found")
+
+
+def command_not_recognaized(cmd: str) -> None:
+    print(f"{cmd}: command not recognaized")
 
 if __name__ == "__main__":
     main()
